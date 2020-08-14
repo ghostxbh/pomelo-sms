7e05543a7a713fd24eed9ca2a2a707a447d11cd4
ghostxbh <ghostxbh@hotmail.com>
2020-08-15 00:51:51 +0800

    add mapper

---
+++ src/main/java/com/uzykj/smsSystem/core/mapper/SmsAccountMapper.java
@@ -0,0 +1,12 @@
+package com.uzykj.smsSystem.core.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.uzykj.smsSystem.core.domain.SmsAccount;
+
+/**
+ * @author ghostxbh
+ * @date 2020/8/14
+ * @description
+ */
+public interface SmsAccountMapper extends BaseMapper<SmsAccount> {
+}
---
+++ src/main/java/com/uzykj/smsSystem/core/mapper/SysUserMapper.java
@@ -0,0 +1,12 @@
+package com.uzykj.smsSystem.core.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.uzykj.smsSystem.core.domain.SysUser;
+
+/**
+ * @author ghostxbh
+ * @date 2020/8/14
+ * @description
+ */
+public interface SysUserMapper extends BaseMapper<SysUser> {
+}
