88af4124ec46c666641535745155b8ccc3f5eccf
ghostxbh <ghostxbh@hotmail.com>
2020-08-15 00:50:49 +0800

    add utils

---
+++ src/main/java/com/uzykj/smsSystem/core/util/DateUtils.java
@@ -0,0 +1,20 @@
+package com.uzykj.smsSystem.core.util;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * @author ghostxbh
+ * @date 2020/8/14
+ * @description
+ */
+public class DateUtils {
+    public static String getBatchNo() {
+        SimpleDateFormat format = new SimpleDateFormat("yyyyMMddHHmmss");
+        return format.format(new Date());
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getBatchNo());
+    }
+}
